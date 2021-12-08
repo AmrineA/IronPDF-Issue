bc30eb4a9a7ac6f803cbb8478418584d414901c2
Adam Amrine <adam.amrine@winonait.com>
2021-12-08 14:28:29 -0500

    Fully demonstrates the issue we're seeing between 2 PDFs that are appear to be identical but are saved in different applications

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using IronPdf;
+
+IronPdf.License.LicenseKey = "";
+var document = new PdfDocument("does_work.pdf");
+document.SaveAs("does_work_output.pdf");
+
+var document2 = new PdfDocument("does_not_work.pdf");
+document2.SaveAs("does_not_work_output.pdf");
