2279b7e752ad97df461df9cb8b3038f7c0c02013
Adam Amrine <adam.amrine@winonait.com>
2022-03-30 11:06:12 -0400

    Issue with DPI specifications not being respected

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿using IronPdf;
+using System.Drawing.Imaging;
 
 IronPdf.License.LicenseKey = "";
-var document = new PdfDocument("does_work.pdf");
-document.SaveAs("does_work_output.pdf");
 
-var document2 = new PdfDocument("does_not_work.pdf");
-document2.SaveAs("does_not_work_output.pdf");
+var document = new PdfDocument("does_work.pdf");
+var bmp = document.ToBitmap();
+using (var stream = new FileStream("AdamTest.72.bmp", FileMode.OpenOrCreate))
+{
+    bmp[0].Save(stream, ImageFormat.Bmp);
+}
+
+bmp = document.ToBitmap(300);
+using (var stream = new FileStream("AdamTest.300.bmp", FileMode.OpenOrCreate))
+{
+    bmp[0].Save(stream, ImageFormat.Bmp);
+}
+
+bmp = document.ToBitmap(600);
+using (var stream = new FileStream("AdamTest.600.bmp", FileMode.OpenOrCreate))
+{
+    bmp[0].Save(stream, ImageFormat.Bmp);
+}
+
